6c2f02dfe8f84ee5f328f8d14b78e586736aa882
Siddhi sahu <himanisahu739@gmail.com>
2025-07-08 12:45:43 +0530

    basics

--- go-basics/main.go
+++ go-basics/main.go
@@ -2,16 +2,60 @@ package main
 
 import "fmt"
 
+import {
+	"log"
+	"os"
+}
+
 func main() {
-	// fmt.Println("this is itt")
-	// anotherFile()
+	//declaring variables
 
-	sum := 0
+	// a := "heeyy"
+ 
+	// fmt.Println(a)
 
-	for sum < 1000 {
-		sum += sum
-		sum = sum + 1
+	// var f string
+	// f = "withhh"
+
+	// fmt.Println(f)
+
+	//Error handling
+	f, err := os.Open("filename.txt")
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	fmt.Println(sum)
+	//Do something with file
+
+	n, err := f.Read()
+
+	if err != nil{
+
+	}
+
+	//Arrays and Slices
+
+	//arrays are static while slices are dynamic
+
+	prime := [6]int{2, 3, 4, 7, 11, 13}
+
+	var s []int = prime[1:4]
+
+	fmt.Println(s)
+
+	//Maps
+	m := make(map[string]int)
+
+	m["ONE"] = 1
+
+	fmt.Println("map", m)
+
+	// for range loop over slices
+
+	k := []int{1, 2, 3, 4, 5}
+
+	for i, v := range k {
+		fmt.Println(i, v)
+	}
 }
